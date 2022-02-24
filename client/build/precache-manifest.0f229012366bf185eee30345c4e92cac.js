5e7b2a511c11d40ef17d2609d8728b826c6c2ba4
unknown <bhmyacine@gmail.com>
2022-02-24 21:34:03 +0100

    add build

---
+++ client/build/precache-manifest.0f229012366bf185eee30345c4e92cac.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48de65d0c228684fedbe3d81a09bd862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e0cce0834ec968ecd6d",
+    "url": "/static/css/main.2d8b901a.chunk.css"
+  },
+  {
+    "revision": "11139c4e56eabba5a886",
+    "url": "/static/js/0.e0f31c0d.chunk.js"
+  },
+  {
+    "revision": "2a543f6739ba376eb91e",
+    "url": "/static/js/1.cf35ef66.chunk.js"
+  },
+  {
+    "revision": "303817f94676ebfa4c65",
+    "url": "/static/js/22.5d3e5eca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/22.5d3e5eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fcc529958cef5baa2e6",
+    "url": "/static/js/23.de94d774.chunk.js"
+  },
+  {
+    "revision": "9d4dd5f74edc2e3609d3",
+    "url": "/static/js/Cart.743c613b.chunk.js"
+  },
+  {
+    "revision": "69d87d9b78d8f2e9e9f4",
+    "url": "/static/js/EventEdit.89deae4b.chunk.js"
+  },
+  {
+    "revision": "0ef9997cc4d2c837e9fb",
+    "url": "/static/js/EventList.938c2f86.chunk.js"
+  },
+  {
+    "revision": "a298d5744e1b3a5b8379",
+    "url": "/static/js/Login.12c8056a.chunk.js"
+  },
+  {
+    "revision": "856d1af33edb0b553622",
+    "url": "/static/js/Order.a9bf16ec.chunk.js"
+  },
+  {
+    "revision": "1dde91074445f7d6d432",
+    "url": "/static/js/OrderList.c1399e24.chunk.js"
+  },
+  {
+    "revision": "53b18599d83cabe67ff0",
+    "url": "/static/js/Payment.2c3b9b14.chunk.js"
+  },
+  {
+    "revision": "6711906fb60b41417b16",
+    "url": "/static/js/PlaceOrder.b0178946.chunk.js"
+  },
+  {
+    "revision": "5231be4957cbecdd2047",
+    "url": "/static/js/Product.c4cc476c.chunk.js"
+  },
+  {
+    "revision": "0c75b4aa775f7ae41e3e",
+    "url": "/static/js/ProductEdit.d59a1477.chunk.js"
+  },
+  {
+    "revision": "a20badd8b289591fe57a",
+    "url": "/static/js/ProductList.4e92c49e.chunk.js"
+  },
+  {
+    "revision": "355727a72b250ab0d0fc",
+    "url": "/static/js/Products.40d7953d.chunk.js"
+  },
+  {
+    "revision": "d0d4f9acee3c61c1d166",
+    "url": "/static/js/Profile.a6e48159.chunk.js"
+  },
+  {
+    "revision": "cc78a9555f412871a37b",
+    "url": "/static/js/SearchResults.3f29db74.chunk.js"
+  },
+  {
+    "revision": "71030300484d23877ad6",
+    "url": "/static/js/Shipping.2c58b565.chunk.js"
+  },
+  {
+    "revision": "0fa5165b955a193d74e0",
+    "url": "/static/js/Signup.339a3ad5.chunk.js"
+  },
+  {
+    "revision": "d153f0211ecd73771890",
+    "url": "/static/js/UserEdit.54d9a393.chunk.js"
+  },
+  {
+    "revision": "277ff7288c3ecb1ad56f",
+    "url": "/static/js/UserList.99d65f61.chunk.js"
+  },
+  {
+    "revision": "5e0cce0834ec968ecd6d",
+    "url": "/static/js/main.09be77c4.chunk.js"
+  },
+  {
+    "revision": "26cfce5df71f7ae60cfb",
+    "url": "/static/js/runtime-main.42025e62.js"
+  }
+]);
